0ae671f4b5eebd5111dd9765ee186ad32a607a36
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2023-12-14 21:56:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,25 +16,3 @@ app.use('/api/user',authRouter)
 app.listen(PORT ,() =>{
     console.log(`Server is running on PORT ${PORT}`)
 });
-
-// const express = require("express");
-// const dbConnect = require("./Config/dbConnect.js");
-// const app = express();
-// const dotenv = require('dotenv').config();
-// const PORT = process.env.PORT || 4000;
-// const authRouter = require('./Routes/authRoute.js');
-// dbConnect();
-// // Use the built-in Express middleware for parsing request bodies
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use('/api/user', authRouter);
-
-// app.use((req, res, next) => {
-//     console.log("Received request body at application level:", req.body);
-//     next();
-//   });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on PORT ${PORT}`);
-// });
