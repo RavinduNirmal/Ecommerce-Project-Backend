6ba48b36c8ae33eb429964a11b2d528408d03090
Ravindu Nirmal <ravindu.nirmal099@gmail.com>
2023-12-17 01:32:42 +0530

    Implementing middlewares to check if the Admin User
    Only if the user is an Admin,can do the product functionality

--- Routes/productRoute.js
+++ Routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { CreateProduct,GetAProduct,FindProducts,updateProduct,deleteProduct } = require("../Controllers/productCtrl");
+const {authMiddleware,isAdmin} = require("../Middlewares/authMiddleare");
 
-router.post("/create", CreateProduct);
+router.post("/create",authMiddleware,isAdmin, CreateProduct);
 router.get("/allproduct", FindProducts);
-router.get("/:id",GetAProduct);
-router.put("/:id",updateProduct);
-router.delete("/:id",deleteProduct);
+router.get("/:id",authMiddleware,isAdmin,GetAProduct);
+router.put("/:id",authMiddleware,isAdmin,updateProduct);
+router.delete("/:id",authMiddleware,isAdmin,deleteProduct);
 module.exports = router;
